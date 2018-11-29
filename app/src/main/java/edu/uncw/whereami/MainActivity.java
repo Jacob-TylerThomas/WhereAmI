e3e76237a99945195614981beede7accbdf6126f
jtt8745 <jacobtylert@gmail.com>
2018-11-29 13:53:39 -0500

    Added in the prompt that asks users to grant the permissions

--- app/src/main/java/edu/uncw/whereami/MainActivity.java
+++ app/src/main/java/edu/uncw/whereami/MainActivity.java
@@ -2,6 +2,7 @@ package edu.uncw.whereami;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -16,6 +17,8 @@ import io.objectbox.Box;
 public class MainActivity extends AppCompatActivity {
 
     RecyclerView mRecyclerView;
+
+    static final int MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION=001;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,12 +38,45 @@ public class MainActivity extends AppCompatActivity {
     public void recordClick(View view) {
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed; request the permission
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION);
+            }
         }
 
         else {
-            Toast.makeText(this, "Please enable location permissions if you would like to access this feature.", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "Permission granted.", Toast.LENGTH_SHORT).show();
 
         }
 
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_ACCESS_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // permission was granted, yay! Do the
+                    // contacts-related task you need to do.
+                } else {
+                    // permission denied, boo! Disable the
+                    // functionality that depends on this permission.
+                }
+                return;
+            }
+
+            // other 'case' lines to check for other
+            // permissions this app might request.
+        }
     }
 }
